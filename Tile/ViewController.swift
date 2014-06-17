191b9f6203af38ef52d42ed99384cf8044dcae19
Lamb <lambgao@me.com>
2014-06-17 22:23:36 +0800

    init

---
+++ Tile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tile
+//
+//  Created by Lamb on 14-6-17.
+//  Copyright (c) 2014年 mynah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
